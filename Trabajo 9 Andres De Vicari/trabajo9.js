c916f07729ecd4dda8d1d4cb2182f6c72f285664
Andrés <tarea.97@live.com.ar>
2022-11-29 21:09:57 -0300

    acá empieza todo

---
+++ Trabajo 9 Andres De Vicari/trabajo9.js
@@ -0,0 +1,11 @@
+let pais = "España" ;
+let continente = "Europa" ;
+let antiguedad = "2021" ;
+let pais_y_continente = pais + " " + continente;
+
+
+pais= "Mexico";
+continente= "Latinoamerica";
+
+console.log(pais, continente, antiguedad);
+alert(pais_y_continente);

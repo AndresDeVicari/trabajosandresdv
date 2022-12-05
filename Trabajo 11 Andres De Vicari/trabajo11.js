baaa155f338bc28a50f125e778ac0f75811d6019
Andrés <tarea.97@live.com.ar>
2022-12-04 22:40:44 -0300

    trabajo11

---
+++ Trabajo 11 Andres De Vicari/trabajo11.js
@@ -0,0 +1,31 @@
+let diaSemana = prompt("¿Qué día es hoy?");
+switch (diaSemana) {
+case "lunes":
+alert("10 abdominales");
+ break;
+case "martes":
+alert("10 sentadillas");
+ break;
+case "miércoles":
+alert("10 estocadas");
+ break;
+case "jueves":
+alert("120 abdominales");
+ break;
+ case "viernes":
+alert("30 flexiones");
+ break;
+case "sábado":
+alert("20 burpis");
+ break;
+case "domingo":
+alert("descansar");
+ break;
+ default:
+alert("Escribe el día de la semana en minúsculas.");
+}
+
+for(i=20;i<=70;i++)
+{
+    document.write("El número es: " + i + "<br>");
+    }

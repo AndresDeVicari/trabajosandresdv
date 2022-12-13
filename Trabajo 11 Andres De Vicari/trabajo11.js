767cc0542ebd53425a3ae9957e7ef413678aa4a9
Andrés <tarea.97@live.com.ar>
2022-12-13 20:44:21 -0300

    tpfinal

--- Trabajo 11 Andres De Vicari/trabajo11.js
+++ Trabajo 11 Andres De Vicari/trabajo11.js
@@ -1,4 +1,4 @@
-let diaSemana = prompt("¿Qué día es hoy?");
+/*let diaSemana = prompt("¿Qué día es hoy?");
 switch (diaSemana) {
 case "lunes":
 alert("10 abdominales");
@@ -28,4 +28,96 @@ alert("Escribe el día de la semana en minúsculas.");
 for(i=20;i<=70;i++)
 {
     document.write("El número es: " + i + "<br>");
-    }
+    }  */
+
+
+
+
+
+/* ejercicio numero 1*/
+
+    var numero1, numero2, numero3, prom;
+
+numero1 = parseInt(prompt("Numero1"));
+numero2 = parseInt(prompt("Numero2"));
+numero3 = parseInt(prompt("Numero3"));
+prom = ((numero1 + numero2 + numero3) /3);
+prom = alert ( "El promedio es: " + prom );
+
+
+
+
+
+
+
+
+
+
+
+   /* ejercicio numero 2
+    let num1, num2, num3, mayor;
+num1 = prompt ("Ingrese el primer numero");
+num2 = prompt ("Ingrese el segundo numero");
+num3 = prompt ("ingrese el tercer numero");
+if(num1 >= num2 & num1 >= num3){
+    mayor = num1;
+} else if(num2 >= num1 & num2 >= num3){
+    mayor = num2;
+} else{
+    mayor = num3;
+}
+alert ("el numero mayor es: " + mayor)
+
+*/
+
+
+
+
+
+
+
+ /*
+ ejercicio numero 3 incompleto
+ let num1, num2, num3, num4, num5;
+
+num1 = prompt ("ingresa el primer numero")
+num2 = prompt ("ingresa el segundo numero")
+num3 = prompt ("ingresa el tercer numero")
+num4 = prompt ("ingresa el cuarto numero")
+num5 = prompt ("ingresa el quinto numero")*/
+
+let colores = ['Rojo','Naranja','Azul'];
+
+colores.push('Violeta');
+
+console.log(colores);
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
